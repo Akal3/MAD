faf3b2452f144e7c9e14b224f8897c7edc430f2f
Akal3 <43915584+Akal3@users.noreply.github.com>
2018-10-06 23:01:12 -0400

    Add files via upload

--- Madlib.js
+++ Madlib.js
@@ -2,11 +2,11 @@ var storynum = Math.random()*10;
 var story;
   switch (storynum) {
     case 1:
-    required = ["A organization name", "noun", "savage adjective", "illness/disease", "verb ending with ing"]
+    required = ["A organization name", "noun", "savage adjective", "illness/disease", "verb ending with ing"];
     story = "There is a rise of a new political group" + userI[1] +
     "They always meet up at the secret and sacred" + userI[2] +
     "in order to feel like they have purpose and to avoid being triggered by society. They are known for being"
     + userI[3] + "as evident by their " + userI[4] + ". As a result of their oddness they love " + userI[5] +
-    "with each other"
+    "with each other";
     break;
     }
